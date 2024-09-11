c0fc7db3c50f74ed36dccb769f54c2b694caa01f
Azfar Shaikh <azfarshaikh7860@gmail.com>
2024-09-11 23:53:03 +0530

    Initial commit with project structure and files

---
+++ server/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className=App>
+      <h1>Railway Navigation System</h1>
+      {/* TODO: Add components here */}
+    </div>
+  );
+}
+
+export default App;
